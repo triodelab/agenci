894369b066ac1a6cea5ff2f0729631ba8bd40bac
Jack Edwin Delamou <jackdelamou.97@gmail.com>
2026-05-27 22:49:43 +0200

    bugfix branch

--- apps/web/app/api/inngest/route.ts
+++
@@ -1,8 +0,0 @@
-import { inngest } from "@/inngest/client";
-import { processWebsiteOnboarding } from "@/modules/files/inngest/website-onboarding";
-import { serve } from "inngest/next";
-
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [processWebsiteOnboarding],
-});
